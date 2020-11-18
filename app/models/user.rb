2001596092a2850bf3794b075a904451483e0ed7
isolate-spark <giordanotemple@gmail.com>
2020-11-17 23:20:23 -0500

    pre-friends implementation 3.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,5 @@ class User < ApplicationRecord
 
   has_many :posts
 
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
-
-  has_many :received_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :received_friends, through: :received_friendships, source: 'user'
-
-  def active_friends
-    friends.select{ |friend| friend.friends.include?(self) }  
-  end
-
-  def pending_friends
-    friends.select{ |friend| !friend.friends.include?(self) }  
-  end
-
   
 end
